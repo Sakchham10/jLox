374c234af27550b494226a18bffe1229c566b8be
Sakchham10 <Sakchham10@Hotmail.com>
2023-12-14 13:02:26 -0600

    Added Ast printer

--- com/Lox/Expr.java
+++ com/Lox/Expr.java
@@ -3,6 +3,13 @@ package com.Lox;
 import java.util.List;
 
 abstract class Expr {
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
+    abstract <R> R accept(Visitor<R> visitor);
  static class Binary extends Expr {
  Binary(Expr left, Token operator, Expr right) {
  this.left = left;
@@ -13,21 +20,37 @@ abstract class Expr {
  final Expr left;
  final Token operator;
  final Expr right;
+     @Override
+     <R> R accept(Visitor<R> visitor) {
+         return visitor.visitBinaryExpr(this);
+     }
+
  }
+
  static class Grouping extends Expr {
  Grouping(Expr expression) {
  this.expression = expression;
 }
 
  final Expr expression;
+     @Override
+     <R> R accept(Visitor<R> visitor) {
+         return visitor.visitGroupingExpr(this);
+     }
  }
+
  static class Literal extends Expr {
  Literal(Object value) {
  this.value = value;
 }
 
  final Object value;
+     @Override
+     <R> R accept(Visitor<R> visitor) {
+         return visitor.visitLiteralExpr(this);
+     }
  }
+
  static class Unary extends Expr {
  Unary(Token operator, Expr right) {
  this.operator = operator;
@@ -36,5 +59,10 @@ abstract class Expr {
 
  final Token operator;
  final Expr right;
+     @Override
+     <R> R accept(Visitor<R> visitor) {
+         return visitor.visitUnaryExpr(this);
+     }
  }
+
 }
